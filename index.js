2706fdec76252e75d3bc37392f0253f9a9073f65
Merseyside-Consultancy <officeimages12@gmail.com>
2023-11-29 11:21:49 +0600

    last commit

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ const port = process.env.PORT || 5000;
 // middleware
 app.use(cors());
 app.use(express.json());
+app.use(cors({ origin: 'http://localhost:5173' }));
 
 // certificates
 // rkEnrhpfvnYfYRqL
